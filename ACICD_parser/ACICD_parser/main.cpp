2f01fe31d921aadd3da24a2bb6ae4d48925e3bf3
nbonnet <nicolas.bonnet@sogeti.com>
2016-02-03 23:48:18 +0100

    Signed-off-by: nbonnet <nicolas.bonnet@sogeti.com>

--- ACICD_parser/ACICD_parser/main.cpp
+++ ACICD_parser/ACICD_parser/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+
+#include "csv.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Hello World!" << endl;
+
+    io::CSVReader<28,io::trim_chars<' '>, io::no_quote_escape<'\t'>, io::single_line_comment<'#'>>  in("acicd.csv");
+    int a;
+    while(in.read_row(a)){
+        cout << "a" + a << endl;
+
+      }
     return 0;
 }
 
