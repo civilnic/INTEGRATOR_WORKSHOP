1ccc52546951480e77be432add480539c4274177
BONNET <nicolas.bonnet@sogeti.com>
2016-06-15 15:54:41 +0200

    ...
    Signed-off-by: BONNET <nicolas.bonnet@sogeti.com>

--- MICD_parser/MICD_parser/main.cpp
+++ MICD_parser/MICD_parser/main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <QString>
 
-#include <libxls/xls.h>
+#include <xls.h>
 
 using namespace std;
+using namespace xls;
 
-int main(int argc, char *argv[])
+int main(void)
 {
+    xlsWorkBook *WorkBook;
+
+    const char *micd_name="ICD_prim_a_1.xls";
+    const char *characterSet="UTF-8";
+
+    WorkBook=xls_open(micd_name,characterSet);
 
     return 0;
 }
