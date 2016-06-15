c1832b1fbe857b71985ac17e75087f3412d723ec
nbonnet <nicolas.bonnet@sogeti.com>
2016-06-16 00:07:36 +0200

    test libxls
    Signed-off-by: nbonnet <nicolas.bonnet@sogeti.com>

--- MICD_parser/MICD_parser/main.cpp
+++ MICD_parser/MICD_parser/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <QString>
 
 #include <xls.h>
 
@@ -11,11 +10,31 @@ using namespace xls;
 int main(void)
 {
     xlsWorkBook *WorkBook;
+    xlsWorkSheet *WorkSheet;
+    int i,t;
+    struct st_row_data* row;
 
-    const char *micd_name="ICD_prim_a_1.xls";
-    const char *characterSet="UTF-8";
+    const char *micd_name="F:\\Mes documents\\Nicolas\\Boulot\\Developpement\\INTEGRATOR_WORKSHOP\\MICD_parser\\MICD_parser\\ICD_prim_a_1.xls";
 
-    WorkBook=xls_open(micd_name,characterSet);
+    WorkBook=xls_open(micd_name,"UTF-8");
 
-    return 0;
+    if (WorkBook!=NULL)
+    {
+        for (i=0;i<WorkBook->sheets.count;i++)
+            printf("Sheet N%i (%s) pos %i\n",i,WorkBook->sheets.sheet[i].name,WorkBook->sheets.sheet[i].filepos);
+
+
+        WorkSheet=xls_getWorkSheet(WorkBook,3);
+
+        xls_parseWorkSheet(WorkSheet);
+        for (t=0;t<=WorkSheet->rows.lastrow;t++)
+        {
+
+        }
+    }
+    else
+    {
+        printf("Impossible d ouvrir %s\n",micd_name);
+    }
+        return 0;
 }
