0835dcb87259004659a2361c5585530f6998ccef
BONNET <nicolas.bonnet@sogeti.com>
2016-09-08 16:02:24 +0200

    MICD parser
    Signed-off-by: BONNET <nicolas.bonnet@sogeti.com>

--- MICD_parser/MICD_parser/micd_document.cpp
+++ MICD_parser/MICD_parser/micd_document.cpp
@@ -13,8 +13,13 @@ micd_document::micd_document(sql_database_manager *database_manager,const char *
     xlsWorkBook *WorkBook;
     xlsWorkSheet *WorkSheet;
 
-    string sheet_name;
-    string header_test_string;
+    xlsCell	*cell=NULL;
+
+    string sheet_name("");
+    string temp("");
+    string header_test_string("");
+
+    std::vector<std::string> *vector_test;
 
     WorkBook=xls_open(filename,"UTF-8");
 
@@ -22,42 +27,112 @@ micd_document::micd_document(sql_database_manager *database_manager,const char *
     {
         for (i=0;i<WorkBook->sheets.count;i++)
         {
+            //
+            // get and display sheet name
+            //
             sheet_name=std::string((char *)WorkBook->sheets.sheet[i].name);
             printf("Sheet N%i (%s) pos %i\n",i,WorkBook->sheets.sheet[i].name,WorkBook->sheets.sheet[i].filepos);
 
+            //
+            //  If sheet name is equal to FUN_IN
+            //
+
             if(sheet_name.compare("FUN_IN")==0)
             {
+                //
+                // get worksheet from workbook
+                //
+
                 WorkSheet=xls_getWorkSheet(WorkBook,i);
 
+                //
+                // parse worksheet
+                //
+
                 xls_parseWorkSheet(WorkSheet);
+
+                //
+                // Display rows & columns numbers
+                //
+
                 printf("Count of rows: %i\n",WorkSheet->rows.lastrow + 1);
                 printf("Max col: %i\n",WorkSheet->rows.lastcol);
 
-                //header=xls_row(WorkSheet,0);
+                //
+                // Get first line = header
+                //      _ Header type is xlsRow
+                //      _ To determine MICD type, it will compared to several header type
+                //      defined in micd_header.h
+                //
                 header=&WorkSheet->rows.row[0];
+
+                //
+                // loop on each col of first row
+                // each cell has a specific type defined by libxls: xlsCell
+                // Here we want to concantenate each cell value in lower case without space to be
+                // compared to header string vectors from micd_header.h
+                //
                 for (tt=0;tt<=WorkSheet->rows.lastcol;tt++)
                 {
-                    xlsCell	*cell;
+                    //
+                    // set cell pointer to current cell
+                    //
                     cell = &header->cells.cell[tt];
-                    if(cell->id == 0x201) continue;
-                    string temp=std::string((char *)cell->str);
 
-                    /* string convert in lower case */
+                    //
+                    // if cell is empty go to next cell;
+                    //
+                    if(cell->id == 0x201) continue;
+
+                    //
+                    // convert cell content into string
+                    //
+                    temp=std::string((char *)cell->str);
+
+                    //
+                    // string convert in lower case
+                    //
                     boost::algorithm::to_lower(temp);
 
-                    /* delete all spaces from string */
+                    //
+                    // delete all spaces from string
+                    //
                     boost::algorithm::erase_all(temp," ");
 
+                    //
+                    // add temp string into concatenation header string
+                    //
                     header_test_string+=temp;
                 }
 
+                // display complete header concatenation string
                 std::cout <<header_test_string << std::endl<< std::endl;
 
-                std::vector<std::string> vector_test=MICD_header_FUN_IN_type2;
-                string test=boost::algorithm::join(vector_test,"");
-                boost::algorithm::to_lower(test);
-                boost::algorithm::erase_all(test," ");
-                std::cout <<test << std::endl;
+                // ierate through test header vector (defined in micd_header.h)
+                for(it_header_test = MICD_header_test.begin(); it_header_test!=MICD_header_test.end(); ++it_header_test)
+                {
+                    // string creation from itrator targetted vector
+                    temp=boost::algorithm::join(*it_header_test,"");
+
+                    // lower conversion
+                    boost::algorithm::to_lower(temp);
+
+                    // delete white space
+                    boost::algorithm::erase_all(temp," ");
+
+                    // compare with micd header test string
+                    if(temp==header_test_string)
+                    {
+                        // save MICD header format into header_vector
+                        vector_test=&(*it_header_test);
+
+                        // TO BE REMOVED: check header vector
+                        temp=boost::algorithm::join(*vector_test,"");
+                        std::cout <<temp << std::endl;
+                    }
+
+                }
+
 
 
                 for (t=0;t<=WorkSheet->rows.lastrow;t++)
--- MICD_parser/MICD_parser/micd_header.h
+++ MICD_parser/MICD_parser/micd_header.h
@@ -5,13 +5,18 @@
 #include <string>
 
 std::vector<std::string> MICD_header_FUN_IN_type1={"Name","Type","Unit","Description","Convention","Dim1","Dim2","Com","Format","Com Mode","From","Refresh","Rate","Min","Max","Enum","Consumed if","Aircraft Signal Name","Interface Level","Status (SSM/FS/Refresh)","Simulation Level [1]","Init Value","Custom","Comment","Last Modification"};
-std::vector<std::string> MICD_header_FUN_IN_type2={"Name","Type","Unit","Description","Convention","Dim1","Dim2","Com","Format","Com Mode","From","Refresh","Rate","Min","Max","Enum","Consumed if","Aircraft Signal Name","Interface Level","Status (SSM/FS/Refresh)","Simulation Level [1]","Simulation Level [2]","Init Value","Custom","Comment","Last Modification"};
+std::vector<std::string> MICD_header_FUN_IN_type2={"Name","Type","Unit","Description","Convention","Dim1","Dim2","Com","Format","Com Mode","From","Refresh","Rate","Min","Max","Enum","Consumed if","Aircraft Signal Name","Interface Level","Status (SSM/FS/Refresh)","Simulation Level [0]","Simulation Level [1]","Init Value","Custom","Comment","Last Modification"};
 std::vector<std::string> MICD_header_FUN_IN_type3={"Name","Type","Unit","Description","Convention","Dim1","Dim2","Com","Format","Com Mode","From","Refresh","Rate","Min","Max","Enum","Consumed if","Aircraft Signal Name","Interface Level","Status (SSM/FS/Refresh)","Simulation Level [1]","Simulation Level [2]","Simulation Level [3]","Init Value","Custom","Comment","Last Modification"};
 std::vector<std::string> MICD_header_FUN_IN_type4={"Name","Type","Unit","Description","Convention","Dim1","Dim2","Com","Format","Com Mode","From","Refresh","Rate","Min","Max","Enum","Consumed if","Aircraft Signal Name","Interface Level","Status (SSM/FS/Refresh)","Simulation Level [1]","Simulation Level [2]","Simulation Level [3]","Simulation Level [4]","Init Value","Custom","Comment","Last Modification"};
+
+std::vector<std::vector<std::string>> MICD_header_test={MICD_header_FUN_IN_type1,MICD_header_FUN_IN_type2,MICD_header_FUN_IN_type3,MICD_header_FUN_IN_type4};
 
 std::vector<std::string> MICD_header_FUN_OUT_type1={"Name","Type","Unit","Description","Convention","Dim1","Dim2","Com Format","Com Mode","To","Refresh","Rate","Min","Max","Enum","Produced if","Aircraft Signal Name","Interface Level","Status (SSM/FS/Refresh)","Simulation Level [1]","Comment","Not Simulated Data","Default Value","Last Modification"};
 std::vector<std::string> MICD_header_FUN_OUT_type2={"Name","Type","Unit","Description","Convention","Dim1","Dim2","Com Format","Com Mode","To","Refresh","Rate","Min","Max","Enum","Produced if","Aircraft Signal Name","Interface Level","Status (SSM/FS/Refresh)","Simulation Level [1]","Simulation Level [2]","Comment","Not Simulated Data","Default Value","Last Modification"};
 std::vector<std::string> MICD_header_FUN_OUT_type3={"Name","Type","Unit","Description","Convention","Dim1","Dim2","Com Format","Com Mode","To","Refresh","Rate","Min","Max","Enum","Produced if","Aircraft Signal Name","Interface Level","Status (SSM/FS/Refresh)","Simulation Level [1]","Simulation Level [2]","Simulation Level [3]","Comment","Not Simulated Data","Default Value","Last Modification"};
 std::vector<std::string> MICD_header_FUN_OUT_type4={"Name","Type","Unit","Description","Convention","Dim1","Dim2","Com Format","Com Mode","To","Refresh","Rate","Min","Max","Enum","Produced if","Aircraft Signal Name","Interface Level","Status (SSM/FS/Refresh)","Simulation Level [1]","Simulation Level [2]","Simulation Level [3]","Simulation Level [4]","Comment","Not Simulated Data","Default Value","Last Modification"};
+
+
+std::vector<std::vector<std::string>>::iterator it_header_test;
 
 #endif // MICD_HEADER_H
